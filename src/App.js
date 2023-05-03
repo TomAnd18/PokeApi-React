1cfc45beced27c9ea74aff839d8f008ddbbc8832
Tomas Regalado Manay <andersonedlp_10@hotmail.com>
2023-05-03 18:49:59 -0300

    browserrouter por router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import { Home } from './pages/Home';
 import { Details } from "./pages/Details";
@@ -6,12 +6,12 @@ import { Details } from "./pages/Details";
 function App() {
   return (
     <div className="App">
-        <Router basename="https://main--dapper-bombolone-8ac5f1.netlify.app">
+        <BrowserRouter>
           <Routes>
               <Route path='/' element={ <Home/> }/>
               <Route path='/details/:id' element={ <Details/> }/>
           </Routes>
-        </Router>
+        </BrowserRouter>
     </div>
   );
 }
